9f3f118f34014e7c4906c3851ffe17e25ae18449
Josue Page <josue.page@tum.de>
2020-04-18 18:33:51 +0200

    rfft and irfft working, need to add options for signal size and onesided

--- fftNd.py
+++ fftNd.py
@@ -5,23 +5,15 @@ import numpy as np
 
 letters = 'abcdefghijklmnopqrst'
 
-def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, signal_sizes=(), is_rfft=False, is_inverse=False):
+def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, is_rfft=False, is_inverse=False):
     
+    # Collect arguments in dictionary for final call
+    args = {'input':input, 'signal_ndim':signal_ndim, 'normalized':normalized}
     # Pointer to function to use
     fft_func = torch.fft
     if is_inverse:
-            fft_func = torch.ifft
+        fft_func = torch.ifft
 
-    # Collect arguments in dictionary for final call
-    args = {'input':input, 'signal_ndim':signal_ndim, 'normalized':normalized}
-
-    # Check if real to complex fft selected and asign arguments
-    if is_rfft:
-        args['onesided'] = onesided
-        fft_func = torch.rfft
-        if is_inverse:
-            fft_func = torch.irfft
-            args['signal_sizes'] = signal_sizes
     # If less or equal to 3 dimensions, use pytorch implementation
     if signal_ndim<=3:
         return fft_func(**args)
@@ -29,8 +21,7 @@ def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, signal_sizes=
 
     dimension_names = ['batch','chan']
     dimension_names.extend([letters[i] for i in range(input.ndim-2)])
-    if is_rfft is False:
-        dimension_names[-1] = 'complex'
+    dimension_names[-1] = 'complex'
     input = input.refine_names(*dimension_names)
     
     original_size = input.shape
@@ -39,34 +30,36 @@ def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, signal_sizes=
     out_result = input
     args['signal_ndim'] = 1
 
-    last_dim = 0
-    if is_rfft is False:
-        last_dim = 1
+    last_dim = 1
 
     for nDim in range(2,len(original_size)-last_dim):
         curr_char = dimension_names[nDim]
         # 1D fft of every dimension indivisually, so atach every other into batch dimension
         new_size = [(dimension_names[i]) for i in range(2,len(original_size)) if dimension_names[i]!=curr_char and dimension_names[i]!='complex'] 
         new_size = ['batch'] + new_size + ['chan',curr_char]
-
-        extra_dims = 2
-        if is_rfft is False or nDim>2:
-            new_size.extend(['complex'])
-            extra_dims = 3
+        new_size.extend(['complex'])
 
         middle_result = out_result.align_to(*new_size)
 
-        middle_size = middle_result.shape
-        batch_size = [middle_result.shape[i] for i in range(middle_result.ndim-extra_dims)]
+        middle_size = list(middle_result.shape)
+        batch_size = [middle_result.shape[i] for i in range(middle_result.ndim-2)]
         batch_size = np.prod(batch_size)
 
-        view_size = [batch_size, original_size[1],original_size[nDim]]
-        if is_rfft is False or nDim>2:
-            view_size.extend([2])
+        view_size = [batch_size, original_size[1],original_size[nDim], 2]
+        
         middle_result = middle_result.contiguous().rename(None).view(view_size)
 
         # apply 1D fft
         args['input'] = middle_result
+
+        # check if it is irfft for last dimension
+        if is_inverse and is_rfft and nDim == len(original_size)-last_dim-1:
+            fft_func = torch.irfft
+            # remove complex dimension
+            middle_size = middle_size[:-1]
+            new_size = new_size[:-1]
+            args['onesided'] = False
+
         middle_result = fft_func(**args)
 
         out_result = middle_result.view(middle_size).refine_names(*new_size)
@@ -81,10 +74,16 @@ def fftNd(input, signal_ndim=1, normalized=False):
     return __fftNd(input, signal_ndim=signal_ndim, normalized=normalized)
 
 def ifftNd(input, signal_ndim=1, normalized=False, signal_sizes=()):
-    return __fftNd(input, signal_ndim=signal_ndim, normalized=normalized, signal_sizes=signal_sizes, is_inverse=True)
+    return __fftNd(input, signal_ndim=signal_ndim, normalized=normalized, is_inverse=True)
 
 def rfftNd(input, signal_ndim=1, normalized=False, onesided=True):
-    return __fftNd(input, signal_ndim=signal_ndim, normalized=normalized, onesided=onesided, is_rfft=True)
+    # Simulate rfft with ffts
+    dims = input.ndim * [1]
+    dims.extend([2])
+    input = input.unsqueeze(input.ndim).repeat(dims)
+    input[...,1] = 0
+    return __fftNd(input, signal_ndim=signal_ndim, normalized=normalized)
 
 def irfftNd(input, signal_ndim=1, normalized=False, onesided=True, signal_sizes=()):
-    return __fftNd(input, signal_ndim=signal_ndim, normalized=normalized, onesided=onesided, signal_sizes=signal_sizes, is_rfft=True, is_inverse=True)
+    result = __fftNd(input, signal_ndim=signal_ndim, normalized=normalized, is_rfft=True, is_inverse=True)
+    return result[...,0]
--- main.py
+++ main.py
@@ -2,7 +2,34 @@ import torch
 from fftNd import *
 import numpy as np
 import time
-torch.manual_seed(1230)
+# torch.manual_seed(1230)
+
+def _cook_nd_args(a, s=None, axes=None, invreal=0):
+    if s is None:
+        shapeless = 1
+        if axes is None:
+            s = list(a.shape)
+        else:
+            s = take(a.shape, axes)
+    else:
+        shapeless = 0
+    s = list(s)
+    if axes is None:
+        axes = list(range(-len(s), 0))
+    if len(s) != len(axes):
+        raise ValueError("Shape and axes have different lengths.")
+    if invreal and shapeless:
+        s[-1] = (a.shape[axes[-1]] - 1) * 2
+    return s, axes
+
+def irfftn(a, s=None, axes=None, norm=None):
+    a = np.asarray(a)
+    s, axes = _cook_nd_args(a, s, axes, invreal=1)
+    for ii in range(len(axes)-1):
+        a = np.fft.ifft(a, s[ii], axes[ii], norm)
+    a = np.fft.irfft(a, s[-1], axes[-1], norm)
+    return a
+
 
 device = "cpu"
 # Create input complex tensor
@@ -18,7 +45,7 @@ x = torch.rand(1,1,5,5,2)
 # test 4D
 nDims = 4
 dim_sizes = [1,1]
-dim_sizes.extend(nDims*[5])
+dim_sizes.extend(nDims*[2])
 dim_sizes.extend([2])
 x = torch.rand(*dim_sizes).to(device)
 
@@ -39,6 +66,55 @@ print("time: " + str(end-start))
 Y = torch.zeros_like(x)
 Y[0,0,:,...,0] = torch.from_numpy(np.real(yNumpy))
 Y[0,0,:,...,1] = torch.from_numpy(np.imag(yNumpy))
+
+diff = abs(y-Y).sum().item()
+# print(y)
+# print(Y)
+print('diff: ' + str(diff))
+
+
+# RFFT
+torch.cuda.synchronize()
+start = time.time()
+y = rfftNd(x[...,0],nDims)
+torch.cuda.synchronize()
+end = time.time()
+print("time: " + str(end-start))
+
+xNumpy = x[0,0,:,...,0].cpu().numpy()
+
+start = time.time()
+yNumpy = np.fft.rfftn(xNumpy)
+end = time.time()
+print("time: " + str(end-start))
+
+Y = torch.zeros_like(x)
+Y[0,0,:,...,0] = torch.from_numpy(np.real(yNumpy))
+Y[0,0,:,...,1] = torch.from_numpy(np.imag(yNumpy))
+
+diff = abs(y-Y).sum().item()
+# print(y)
+# print(Y)
+print('diff: ' + str(diff))
+
+
+# irfft
+torch.cuda.synchronize()
+start = time.time()
+y = irfftNd(x,nDims)
+torch.cuda.synchronize()
+end = time.time()
+print("time: " + str(end-start))
+
+xNumpy = x[0,0,:,...,0].cpu().numpy() + x[0,0,:,...,1].cpu().numpy()* 1j
+
+start = time.time()
+yNumpy = irfftn(xNumpy)
+end = time.time()
+print("time: " + str(end-start))
+
+Y = torch.zeros_like(x[...,0])
+Y[0,0,:,...] = torch.from_numpy(np.real(yNumpy))
 
 diff = abs(y-Y).sum().item()
 # print(y)
