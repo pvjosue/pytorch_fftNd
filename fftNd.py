20bcc33533479b548eaf477f0c990bd0ea89f581
Josue Page <josue.page@tum.de>
2020-04-21 19:04:40 +0200

    fftNd convolution implemented, and all code commented

--- fftNd.py
+++ fftNd.py
@@ -18,7 +18,7 @@ def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, is_rfft=False
     if signal_ndim<=3:
         return fft_func(**args)
 
-
+    # Assign names to dimensions for easier permuting
     dimension_names = ['batch','chan']
     dimension_names.extend([letters[i] for i in range(input.ndim-2)])
     dimension_names[-1] = 'complex'
@@ -27,35 +27,36 @@ def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, is_rfft=False
     original_size = input.shape
     dims_ids = [n for n in range(len(original_size))]
 
+    # Set signal dimention to 1, as nD fourier is performed by n sucesive 1D ffts
     out_result = input
     args['signal_ndim'] = 1
-
     last_dim = 1
 
+    # Iterate dimensions
     for nDim in range(2,len(original_size)-last_dim):
         curr_char = dimension_names[nDim]
         # 1D fft of every dimension indivisually, so atach every other into batch dimension
         new_size = [(dimension_names[i]) for i in range(2,len(original_size)) if dimension_names[i]!=curr_char and dimension_names[i]!='complex'] 
-        new_size = ['batch'] + new_size + ['chan',curr_char]
-        new_size.extend(['complex'])
+        new_size = ['batch'] + new_size + ['chan',curr_char,'complex']
 
+        # Permute such that all dimensions are stacked to the batch dimension, except the nDim
         middle_result = out_result.align_to(*new_size)
 
+        # Compute view shape to run fft 1D on
         middle_size = list(middle_result.shape)
         batch_size = [middle_result.shape[i] for i in range(middle_result.ndim-2)]
         batch_size = np.prod(batch_size)
-
         view_size = [batch_size, original_size[1],original_size[nDim], 2]
-        
+        # And reshape
         middle_result = middle_result.contiguous().rename(None).view(view_size)
 
-        # apply 1D fft
+        # Update arguments for fft
         args['input'] = middle_result
 
-        # check if it is irfft for last dimension
+        # Check if it is irfft for last dimension
         if is_inverse and is_rfft and nDim == len(original_size)-last_dim-1:
             fft_func = torch.irfft
-            # remove complex dimension
+            # Remove complex dimension
             middle_size = middle_size[:-1]
             new_size = new_size[:-1]
             args['onesided'] = onesided
@@ -63,16 +64,18 @@ def __fftNd(input, signal_ndim=1, normalized=False, onesided=True, is_rfft=False
                 middle_size[-1] += middle_size[-1]//2
         if is_inverse == False and is_rfft and nDim == len(original_size)-last_dim-1:
             fft_func = torch.irfft
-            # remove complex dimension
+            # Remove complex dimension
             middle_size = middle_size[:-1]
             new_size = new_size[:-1]
             args['onesided'] = False
 
+        # Run fft_func
         middle_result = fft_func(**args)
 
+        # Get back to original shape
         out_result = middle_result.view(middle_size).refine_names(*new_size)
         out_result = out_result.align_as(input)
-
+    # Remove dimention names and return result
     return out_result.rename(None)
 
 
--- main.py
+++ main.py
@@ -2,53 +2,29 @@ import torch
 from fftNd import *
 import numpy as np
 import time
-# torch.manual_seed(1230)
+torch.manual_seed(1230)
 
-def _cook_nd_args(a, s=None, axes=None, invreal=0):
-    if s is None:
-        shapeless = 1
-        if axes is None:
-            s = list(a.shape)
-        else:
-            s = take(a.shape, axes)
-    else:
-        shapeless = 0
-    s = list(s)
-    if axes is None:
-        axes = list(range(-len(s), 0))
-    if len(s) != len(axes):
-        raise ValueError("Shape and axes have different lengths.")
-    if invreal and shapeless:
-        s[-1] = (a.shape[axes[-1]] - 1) * 2
-    return s, axes
-
-def irfftn(a, s=None, axes=None, norm=None):
-    a = np.asarray(a)
-    s, axes = _cook_nd_args(a, s, axes, invreal=1)
-    for ii in range(len(axes)-1):
-        a = np.fft.ifft(a, s[ii], axes[ii], norm)
-    a = np.fft.irfft(a, s[-1], axes[-1], norm)
-    return a
-
-
-device = "cpu"
+device = "cuda"
 # Create input complex tensor
 x = torch.rand(1,1,5,5,2)
 
 
 # Test wrappers with 2D
-# y = rfftNd(x,2)
-# y = ifftNd(y,2)
-# y = rfftNd(x[:,:,:,:,0],2)
-# y = irfftNd(y,2)
+y = rfftNd(x,2)
+y = ifftNd(y,2)
+y = rfftNd(x[:,:,:,:,0],2)
+y = irfftNd(y,2)
 
-# test 4D
+############# test 4D convs
 nDims = 6
 dim_sizes = [1,1]
-dim_sizes.extend(nDims*[33])
+dim_sizes.extend(nDims*[5])
 dim_sizes.extend([2])
+# Create complex input tensor
 x = torch.rand(*dim_sizes).to(device)
 
+######### fftNd
+# Run proposed implementation
 torch.cuda.synchronize()
 start = time.time()
 y = fftNd(x,nDims)
@@ -56,23 +32,25 @@ torch.cuda.synchronize()
 end = time.time()
 print("time: " + str(end-start))
 
+# Create Numpy tensor
 xNumpy = x[0,0,:,...,0].cpu().numpy() + x[0,0,:,...,1].cpu().numpy()* 1j
 
+# Run numpy implementation
 start = time.time()
 yNumpy = np.fft.fftn(xNumpy)
 end = time.time()
 print("time: " + str(end-start))
 
+# Copy result to tensor
 Y = torch.zeros_like(x)
 Y[0,0,:,...,0] = torch.from_numpy(np.real(yNumpy))
 Y[0,0,:,...,1] = torch.from_numpy(np.imag(yNumpy))
 
+# Compute error
 diff = abs(y-Y).sum().item()
-# print(y)
-# print(Y)
-print('\tdiff: ' + str(diff))
+print('\tfftNd diff: ' + str(diff))
 
-# IFFT
+########## ifftNd
 torch.cuda.synchronize()
 start = time.time()
 y = ifftNd(x,nDims)
@@ -80,24 +58,26 @@ torch.cuda.synchronize()
 end = time.time()
 print("time: " + str(end-start))
 
+# Create Numpy tensor
 xNumpy = x[0,0,:,...,0].cpu().numpy() + x[0,0,:,...,1].cpu().numpy()* 1j
 
+# Run numpy implementation
 start = time.time()
 yNumpy = np.fft.ifftn(xNumpy)
 end = time.time()
 print("time: " + str(end-start))
 
+# Copy result to tensor
 Y = torch.zeros_like(x)
 Y[0,0,:,...,0] = torch.from_numpy(np.real(yNumpy))
 Y[0,0,:,...,1] = torch.from_numpy(np.imag(yNumpy))
 
+# Compute error
 diff = abs(y-Y).sum().item()
-# print(y)
-# print(Y)
-print('\tdiff: ' + str(diff))
+print('\tifftNd diff: ' + str(diff))
 
 
-# RFFT
+########## rfftNd
 torch.cuda.synchronize()
 start = time.time()
 y = rfftNd(x[...,0],nDims,onesided=True)
@@ -105,24 +85,26 @@ torch.cuda.synchronize()
 end = time.time()
 print("time: " + str(end-start))
 
+# Create Numpy tensor
 xNumpy = x[0,0,:,...,0].cpu().numpy()
 
+# Run numpy implementation
 start = time.time()
 yNumpy = np.fft.rfftn(xNumpy)
 end = time.time()
 print("time: " + str(end-start))
 
+# Copy result to tensor
 Y = torch.zeros_like(y)
 Y[0,0,:,...,0] = torch.from_numpy(np.real(yNumpy))
 Y[0,0,:,...,1] = torch.from_numpy(np.imag(yNumpy))
 
+# Compute error
 diff = abs(y-Y).sum().item()
-# print(y)
-# print(Y)
-print('\tdiff: ' + str(diff))
+print('\trfftNd diff: ' + str(diff))
 
 
-# irfft
+########## irfftNd
 torch.cuda.synchronize()
 start = time.time()
 y = irfftNd(x,nDims, onesided=False)
@@ -130,17 +112,75 @@ torch.cuda.synchronize()
 end = time.time()
 print("time: " + str(end-start))
 
+# Create Numpy tensor
 xNumpy = x[0,0,:,...,0].cpu().numpy() + x[0,0,:,...,1].cpu().numpy()* 1j
 
+# Run numpy implementation
 start = time.time()
-yNumpy = irfftn(xNumpy, xNumpy.shape)#[...,:xNumpy.shape[-1]]
+yNumpy = np.fft.irfftn(xNumpy, xNumpy.shape)
 end = time.time()
 print("time: " + str(end-start))
 
+# Copy result to tensor
 Y = torch.zeros_like(y)
 Y[0,0,:,...] = torch.from_numpy(np.real(yNumpy))
 
+# Compute error
 diff = abs(y-Y).sum().item()
-# print(y)
-# print(Y)
-print('\tdiff: ' + str(diff))
+print('\tirfftNd diff: ' + str(diff))
+
+
+
+######### convNd with fourier
+def mulComplex(x, other):
+    outR = torch.sub(torch.mul(x[...,0], other[...,0]), torch.mul(x[...,1], other[...,1]))
+    outI = torch.add(torch.mul(x[...,0], other[...,1]), torch.mul(x[...,1], other[...,0]))
+    out = torch.cat((outR.unsqueeze(-1), outI.unsqueeze(-1)), outR.ndimension())
+    return out
+
+def fft_conv(A,B):
+    nDims = A.ndim-2
+    padSize = (torch.tensor(A.shape[2:])-torch.tensor(B.shape[2:]))
+    padSizes = torch.zeros(2*nDims,dtype=int)
+    padSizes[0::2] = torch.floor(padSize/2.0)
+    padSizes[1::2] = torch.ceil(padSize/2.0)
+    padSizes = list(padSizes.numpy()[::-1])
+    B = F.pad(B,padSizes)
+    return ifftNd( mulComplex(rfftNd(A, nDims,onesided=False),rfftNd(B, nDims,onesided=False)), nDims)[...,0]
+
+def np_fftconvolve(A, B):
+    nDims = A.ndim-2
+    padSize = (torch.tensor(A.shape[2:])-torch.tensor(B.shape[2:]))
+    padSizes = torch.zeros(2*nDims,dtype=int)
+    padSizes[0::2] = torch.floor(padSize/2.0)
+    padSizes[1::2] = torch.ceil(padSize/2.0)
+    padSizes = list(padSizes.numpy()[::-1])
+    B = F.pad(B,padSizes)
+    A = A[0,0,...].numpy()
+    B = B[0,0,...].numpy()
+    return np.real(np.fft.ifftn(np.fft.fftn(A)*np.fft.fftn(B, s=A.shape)))
+
+# Create image and kernel
+A = torch.rand(1,1,5,6,7,8)
+B = torch.rand(1,1,2,3,4,5)
+
+# Run proposed implementation
+start = time.time()
+C = fft_conv(A,B)
+end = time.time()
+print("time: " + str(end-start))
+
+# Create Numpy tensors
+Anumpy = A[0,0,...].numpy()
+Bnumpy = B[0,0,...].numpy()
+
+# Run numpy implementation
+start = time.time()
+Cnumpy = np_fftconvolve(A,B)
+end = time.time()
+print("time: " + str(end-start))
+Cnumpy = torch.from_numpy(Cnumpy).unsqueeze(0).unsqueeze(0)
+
+# Compute error
+diff = abs(C-Cnumpy).sum().item()
+print('\t Fourier Conv. diff: ' + str(diff))
